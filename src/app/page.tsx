c206bfc335485b77f3449e20575f79e6f7f1930b
uihyeon_kim <57362030+kuh97@users.noreply.github.com>
2025-08-19 19:33:28 +0900

    [feat] Footer 구현
    - 저작권 표기를 포함한 Footer 구현
    - `page.tsx` 구조 수정
      - `Hero`, `About`, `Skills`, `Projects` 가 main태그 내부에 위치하도록 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,25 @@
 import { Header } from "@/components/layout/Header";
 import { ScrollToTop } from "@/components/layout/ScrollToTop";
 import { About } from "@/components/sections/About";
+import { Footer } from "@/components/sections/Footer";
 import { Hero } from "@/components/sections/Hero";
 import { Projects } from "@/components/sections/Projects";
 import { Skills } from "@/components/sections/Skills";
 
 export default function Home() {
   return (
-    <main className="min-h-screen">
+    <div className="min-h-screen">
       <Header />
-      <Hero />
-      <About />
-      <Skills />
-      <Projects />
+
+      <main>
+        <Hero />
+        <About />
+        <Skills />
+        <Projects />
+      </main>
+
       <ScrollToTop />
-    </main>
+      <Footer />
+    </div>
   );
 }
---
+++ src/components/sections/Footer.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export function Footer() {
+  return (
+    <footer className="bg-blue-300 dark:bg-gray-800 text-white text-center py-8">
+      Copyright 2025. KIM UIHYEON. All rights reserved.
+    </footer>
+  );
+}
