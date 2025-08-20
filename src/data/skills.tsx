9772f40bda5f0b8ed9c73f923bf85dc9ad76c3e7
uihyeon_kim <rladml91gus@naver.com>
2025-08-20 19:23:25 +0900

    [feat] Skill 섹션 블러 처리 UX 적용 - 기존 옵션에 따라 스킬 목록이 보이도록 하던 것을 해당 옵션의 스킬들만   보이고 나머지는 blur처리되도록 UX 수정 - `react-icons`를 이미지 파일로 교체

--- src/data/skills.tsx
+++ src/data/skills.tsx
@@ -1,87 +1,144 @@
-import {
-  FaReact,
-  FaHtml5,
-  FaCss3,
-  FaJava,
-  FaGithub,
-  FaSlack,
-  FaDatabase,
-} from "react-icons/fa";
-import { IoLogoJavascript } from "react-icons/io5";
-import { BiLogoTypescript, BiLogoFigma } from "react-icons/bi";
-import {
-  RiNextjsFill,
-  RiTailwindCssFill,
-  RiBearSmileFill,
-} from "react-icons/ri";
-import { SiMobx, SiOracle, SiVercel } from "react-icons/si";
-import { AiFillGitlab } from "react-icons/ai";
 import { SkillsData } from "@/types";
+import Image from "next/image";
+
+const ICONS = {
+  CSS: (
+    <Image src={"/asserts/CSS.svg"} alt={"CSS Icon"} width={36} height={36} />
+  ),
+  FIGMA: (
+    <Image
+      src={"/asserts/Figma.svg"}
+      alt={"Figma Icon"}
+      width={36}
+      height={36}
+    />
+  ),
+  GITHUB: (
+    <Image
+      src={"/asserts/GitHub.svg"}
+      alt={"GitHub Icon"}
+      width={36}
+      height={36}
+    />
+  ),
+  GITLAB: (
+    <Image
+      src={"/asserts/GitLab.svg"}
+      alt={"GitLab Icon"}
+      width={36}
+      height={36}
+    />
+  ),
+  HTML: (
+    <Image src={"/asserts/HTML.svg"} alt={"HTML Icon"} width={36} height={36} />
+  ),
+  JAVA: (
+    <Image src={"/asserts/Java.svg"} alt={"Java Icon"} width={36} height={36} />
+  ),
+  JAVASCRIPT: (
+    <Image
+      src={"/asserts/JavaScript.svg"}
+      alt={"JavaScript Icon"}
+      width={36}
+      height={36}
+    />
+  ),
+  NEXTJS: (
+    <Image
+      src={"/asserts/NextJS.svg"}
+      alt={"NextJS Icon"}
+      width={36}
+      height={36}
+    />
+  ),
+  REACT: (
+    <Image
+      src={"/asserts/React.svg"}
+      alt={"React Icon"}
+      width={36}
+      height={36}
+    />
+  ),
+  TAILWIND: (
+    <Image
+      src={"/asserts/TailwindCSS.svg"}
+      alt={"TailwindCSS Icon"}
+      width={36}
+      height={36}
+    />
+  ),
+  TYPESCRIPT: (
+    <Image
+      src={"/asserts/TypeScript.svg"}
+      alt={"TypeScript Icon"}
+      width={36}
+      height={36}
+    />
+  ),
+  VERCEL: (
+    <Image
+      src={"/asserts/Vercel.svg"}
+      alt={"Vercel Icon"}
+      width={36}
+      height={36}
+    />
+  ),
+  ZUSTAND: (
+    <Image
+      src={"/asserts/Zustand.png"}
+      alt={"Zustnad Icon"}
+      width={36}
+      height={36}
+    />
+  ),
+  ORACLE: (
+    <Image
+      src={"/asserts/Oracle.svg"}
+      alt={"Oracle Icon"}
+      width={36}
+      height={36}
+    />
+  ),
+  SLACK: (
+    <Image
+      src={"/asserts/Slack.svg"}
+      alt={"Slack Icon"}
+      width={36}
+      height={36}
+    />
+  ),
+  MOBX: (
+    <Image src={"/asserts/MobX.svg"} alt={"MobX Icon"} width={36} height={36} />
+  ),
+  JEST: (
+    <Image src={"/asserts/Jest.svg"} alt={"Jest Icon"} width={36} height={36} />
+  ),
+};
 
 export const skills: SkillsData = {
+  All: [],
   Frontend: [
-    {
-      id: 1,
-      name: "JavaScript",
-      icon: <IoLogoJavascript className="text-yellow-400" />,
-    },
-    {
-      id: 2,
-      name: "TypeScript",
-      icon: <BiLogoTypescript className="text-blue-500" />,
-    },
-    { id: 3, name: "HTML5", icon: <FaHtml5 className="text-orange-500" /> },
-    { id: 4, name: "CSS", icon: <FaCss3 className="text-blue-500" /> },
-    { id: 5, name: "React", icon: <FaReact className="text-cyan-400" /> },
-    {
-      id: 6,
-      name: "Next.js",
-      icon: <RiNextjsFill className="text-black dark:text-white" />,
-    },
-    {
-      id: 7,
-      name: "Tailwind",
-      icon: <RiTailwindCssFill className="text-cyan-500" />,
-    },
-    { id: 8, name: "Mobx", icon: <SiMobx className="text-orange-500" /> },
-    {
-      id: 9,
-      name: "Zustand",
-      icon: <RiBearSmileFill className="text-amber-700" />,
-    },
+    { id: 1, name: "JavaScript", icon: ICONS.JAVASCRIPT },
+    { id: 2, name: "TypeScript", icon: ICONS.TYPESCRIPT },
+    { id: 3, name: "HTML5", icon: ICONS.HTML },
+    { id: 4, name: "CSS", icon: ICONS.CSS },
+    { id: 5, name: "React", icon: ICONS.REACT },
+    { id: 6, name: "Next.js", icon: ICONS.NEXTJS },
+    { id: 7, name: "Tailwind", icon: ICONS.TAILWIND },
+    { id: 8, name: "Mobx", icon: ICONS.MOBX },
+    { id: 9, name: "Zustand", icon: ICONS.ZUSTAND },
+    { id: 18, name: "Jest", icon: ICONS.JEST },
   ],
-  Backend: [
-    { id: 10, name: "Java", icon: <FaJava className="text-red-500" /> },
-  ],
+  Backend: [{ id: 10, name: "Java", icon: ICONS.JAVA }],
   Database: [
-    {
-      id: 11,
-      name: "Tibero",
-      icon: <FaDatabase className="text-black dark:text-white" />,
-    },
-    { id: 12, name: "Oracle", icon: <SiOracle className="text-red-600" /> },
+    { id: 11, name: "Tibero", icon: "" },
+    { id: 12, name: "Oracle", icon: ICONS.ORACLE },
   ],
   Others: [
-    {
-      id: 13,
-      name: "GitHub",
-      icon: <FaGithub className="text-black dark:text-white" />,
-    },
-    {
-      id: 14,
-      name: "GitLab",
-      icon: <AiFillGitlab className="text-orange-600" />,
-    },
-    {
-      id: 15,
-      name: "Figma",
-      icon: <BiLogoFigma className="text-purple-500" />,
-    },
-    { id: 16, name: "Slack", icon: <FaSlack className="text-purple-600" /> },
-    {
-      id: 17,
-      name: "Vercel",
-      icon: <SiVercel className="text-black dark:text-white" />,
-    },
+    { id: 13, name: "GitHub", icon: ICONS.GITHUB },
+    { id: 14, name: "GitLab", icon: ICONS.GITLAB },
+    { id: 15, name: "Figma", icon: ICONS.FIGMA },
+    { id: 16, name: "Slack", icon: ICONS.SLACK },
+    { id: 17, name: "Vercel", icon: ICONS.VERCEL },
   ],
 };
