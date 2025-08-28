b79ad2f14799d2719fbff8ffd17edffcdbda103b
uihyeon_kim <rladml91gus@naver.com>
2025-08-28 22:45:10 +0900

    [feat] DB Table Schema 수정 - 프로젝트 Data관련 db table schema 수정 - prisma client 수정   - DB 커넥션 수를 효율적으로 관리하기 위해 Prisma Client 인스턴스를 하나만 생성하여 사용하도록 설정

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,5 +1,18 @@
 import { PrismaClient } from "@prisma/client";
 
-const prisma: PrismaClient = new PrismaClient();
+declare global {
+  // allow global `var` declarations
+  // eslint-disable-next-line no-var
+  var prisma: PrismaClient | undefined;
+}
+
+const prisma =
+  global.prisma ||
+  new PrismaClient({
+    // 개발 중에 실행되는 쿼리를 로그로 보려면 아래 주석을 해제하세요.
+    // log: ["query"],
+  });
+
+if (process.env.NODE_ENV !== "production") global.prisma = prisma;
 
 export default prisma;
