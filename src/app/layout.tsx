4d931dc9a076bfd9e9460582943135c101b45ee5
uihyeon_kim <rladml91gus@naver.com>
2025-09-10 18:19:44 +0900

    [feat] sitemap 추가 및 접근성 개선 - 검색 엔진 최적(SEO) 및 웹 접근성(a11y)을 향상시키기 위해 다음과 같은   사항 적용   - 사이트맵(sitemap.xml) 생성     - Next.js의 `sitemap.ts`를 이용하여 동적으로 사이트맵 생성     - 검색 엔진 크롤러가 사이트의 모든 페이지(메인, 프로젝트 상세 페이지 등)를 효과적으로 발견하고 인덱싱할 수 있도록 설정   - 웹 접근성(a11y) 개선     - 버튼에 `aria-label`을 추가하여 접근성 강화

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,8 @@ import { Footer } from "@/components/layout/Footer";
 import { ScrollToTop } from "@/components/layout/ScrollToTop";
 import { ThemeProvider } from "@/components/providers/ThemeProvider";
 import { SITE_CONFIG } from "@/types";
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -48,6 +50,8 @@ export default function RootLayout({
             <Footer />
           </div>
         </ThemeProvider>
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
